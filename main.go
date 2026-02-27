e7448367f7713d543ec48a962b7e33efd49e6a16
noClaps <git@zerolimits.dev>
2026-02-27 11:03:48 +0000

    Add better help menu

--- main.go
+++ main.go
@@ -4,10 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
+	flag.Usage = func() {
+		w := flag.CommandLine.Output()
+		fmt.Fprintf(w, `Usage: %s <url>
+
+Arguments:
+  url
+        The DOI identifier or URL to fetch data from.
+`, filepath.Base(os.Args[0]))
+	}
+
 	flag.Parse()
 
 	url := flag.Arg(0)
