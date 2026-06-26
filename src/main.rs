f69c6301f0831f3d667ad34631ed6b1197fd3e23
noClaps <git@zerolimits.dev>
2026-06-26 12:21:31 +0100

    Use proper error handling and better error messages

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,34 @@ fn main() {
         }
     );
 
-    let reference: Reference = minreq::get(format!("https://citation.doi.org/metadata?doi={doi}"))
-        .send()?
-        .json()?;
+    let reference: Reference =
+        match minreq::get(format!("https://citation.doi.org/metadata?doi={doi}")).send() {
+            Ok(res) if res.status_code != 200 => {
+                eprintln!("DOI not found: {doi}");
+                exit(1);
+            }
+            Ok(res) => match res.json() {
+                Ok(json) => json,
+                Err(err) => match err {
+                    minreq::Error::SerdeJsonError(err) => {
+                        eprintln!("Error parsing JSON body: {err}");
+                        eprintln!("URL: https://citation.doi.org/metadata?doi={doi}");
+                        eprintln!("Response: {}", res.as_str().unwrap());
+                        exit(1)
+                    }
+                    minreq::Error::InvalidUtf8InBody(err) => {
+                        eprintln!("URL: https://citation.doi.org/metadata?doi={doi}");
+                        eprintln!("Invalid UTF-8 in response: {err}");
+                        exit(1);
+                    }
+                    _ => unreachable!(),
+                },
+            },
+            Err(err) => {
+                eprintln!("Error during request: {err}");
+                exit(1);
+            }
+        };
 
     println!("{reference}");
-    Ok(())
 }
